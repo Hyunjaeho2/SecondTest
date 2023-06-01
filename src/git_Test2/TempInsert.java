87b361789e077c341d0866faec9a9121d301e974
HyunJaeHo <hjh9986@gmail.com>
2023-06-01 17:10:50 +0900

    팀장 최초 파일

---
+++ src/git_Test2/TempInsert.java
@@ -0,0 +1,8 @@
+package git_Test2;
+
+public class TempInsert {
+
+	public void tempInsert(){
+		System.out.println("임시 파일");
+	}
+}
---
+++ src/git_Test2/gitTestMain.java
@@ -0,0 +1,10 @@
+package git_Test2;
+
+public class gitTestMain {
+
+	public static void main(String[] args) {
+		System.out.println("ÆÀ¿ø ÆÀÀå ±³Ã¼");
+
+	}
+
+}
